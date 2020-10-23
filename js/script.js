149fe5cb76fc596996649fb115a270e9a02d1bcf
Nikanora <37082879+Nikanorra@users.noreply.github.com>
2020-10-23 18:15:05 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+	$(function(){
+
+	  $('.slider__wrapper').slick({
+		  dots: true,
+		  infinite: true,
+		  speed: 1000,
+			autoplaySpeed: 5000,
+		  fade: true,
+		  cssEase: 'linear',
+			autoplay: true,
+			mobileFirst: true,
+
+		});
+
+	});
